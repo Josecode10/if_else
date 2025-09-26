5b049e1158e88eb760109872b942b5e9520bd045
Luisa <chema1987@hotmail.com>
2025-09-26 12:15:04 -0500

    ejercicio 4

---
+++ src/modelo/Clase4.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Clase4 {
+    public double calcularAreaTriangulo(double base, double altura) {
+        return (base * altura) / 2;
+    }
+
+    public double calcularAreaCirculo(double radio) {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+}
---
+++ src/vista/Vista4.java
@@ -0,0 +1,19 @@
+package vista;
+
+import java.util.Scanner;
+
+public class Vista4 {
+    public static void main(String[] args) {
+        // Crear objeto de la clase scanner
+        Scanner miScanner = new Scanner(System.in);
+
+        System.out.print("""
+                ¿Qué quieres hacer?
+                1) Calcular área de un triángulo.
+                2) Calcular área de un círculo.
+                """;);
+
+        
+    }
+
+}
