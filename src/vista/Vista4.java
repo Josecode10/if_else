fab005eb19a2811709db1f6bdd214d26076b141b
Luisa <chema1987@hotmail.com>
2025-09-26 12:24:57 -0500

    ejercicio 4

--- src/vista/Vista4.java
+++ src/vista/Vista4.java
@@ -7,11 +7,22 @@ public class Vista4 {
         // Crear objeto de la clase scanner
         Scanner miScanner = new Scanner(System.in);
 
+        var opcion = 0;
         System.out.print("""
-                ¿Qué quieres hacer?
+                \n¿Qué quieres hacer?
                 1) Calcular área de un triángulo.
                 2) Calcular área de un círculo.
-                """;);
+                Escoger una opcion:\s""");
+        
+        try {
+            opcion = Integer.parseInt(miScanner.nextLine());
+        } catch (Exception e) {
+            System.out.print("");
+        }
+
+        if (opcion == 1) {
+            
+        }
 
         
     }
